ccc9b14d94e5e3d4fb4afc5b8adbc64dd158f2bb
sdi0700224 <v.roussos.insurance@gmail.com>
2023-06-19 04:01:25 +0300

    Get filepath as an argument

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "dataimporter.h"
-
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -9,10 +9,17 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    DataImporter dataImporter(nullptr, "C:\\Users\\rouss\\Documents\\Qt Projects\\Renderer\\waveform-data.txt");
+    if (argc != 2)
+    {
+        qDebug() << "Arguments number should be 1";
+        return EXIT_FAILURE;
+    }
+
+    DataImporter dataImporter(nullptr, argv[1]);
 
     if (!dataImporter.Import())
     {
+        qDebug() << "Data could not be Imported from file";
         return EXIT_FAILURE;
     }
 
